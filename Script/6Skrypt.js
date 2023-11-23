cdb8b4afc0860f28173a36d59ae83ed18410a0c2
Piotr <120832758+lordpe333@users.noreply.github.com>
2023-11-23 10:06:09 +0100

    Add files via upload

---
+++ Script/6Skrypt.js
@@ -0,0 +1,5 @@
+const expressionFunction = (x, n) => Math.pow(x, n);
+
+const result = expressionFunction(5, 2); 
+
+console.log(result);
---
+++ Script/7Skrypt.js
@@ -0,0 +1,5 @@
+const pow = (x, n) => Math.pow(x, n);
+
+const result = pow(5, 2); 
+
+console.log(result);
